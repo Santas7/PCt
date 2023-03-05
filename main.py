4451a9456e95e3280f75b52ff91cb27ac569b514
Andrey_Panyshkin <86359412+Santas7@users.noreply.github.com>
2023-03-05 18:27:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,45 @@ class MainWindow(QMainWindow):
         # settings window MainWindow
         self.setWindowTitle("PCt")
         self.setFixedSize(QSize(800, 600)) # size window
-        self.label = QLabel("", self)
+        self.title = QLabel("PCt", self)
+
+        # standart text
         font = QtGui.QFont()
         font.setFamily('CeraPro-Bold')  # font
         font.setPointSize(14)  # size of font
+
+        # title text
+        font_ = QtGui.QFont()
+        font_.setFamily('CeraPro-Bold')  # font
+        font_.setPointSize(27)  # size of font
+
+        # change icon window
         app_icon = QtGui.QIcon()
         app_icon.addFile('icon/PCt.png', QtCore.QSize(256, 256))
         app.setWindowIcon(app_icon)
+
+        # change for title
+        self.title.setFont(font_)
+        self.title.move(380, 50)
+
+        # panel input user name
+        self.input_user = QLineEdit('', self)
+        self.input_user.move(200, 110)
+        self.input_user.setFixedSize(QSize(400, 40))
+        self.input_user.setFont(font)
+
+        # panel input ip
+        self.input_user = QLineEdit('', self)
+        self.input_user.move(200, 160)
+        self.input_user.setFixedSize(QSize(400, 40))
+        self.input_user.setFont(font)
+
+        # panel input password connect
+        self.input_user = QLineEdit('', self)
+        self.input_user.move(200, 210)
+        self.input_user.setFixedSize(QSize(400, 40))
+        self.input_user.setFont(font)
+        
         self.show()
 
 if __name__ == "__main__":
