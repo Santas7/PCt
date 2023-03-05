17e22e112a9fd4f5c8eb3e84113f8b5d9b8b4bde
Andrey_Panyshkin <86359412+Santas7@users.noreply.github.com>
2023-03-05 17:12:06 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QGridLayout, QLineEdit, \
+    QVBoxLayout, QTextEdit, QProgressBar
+from PyQt6 import QtGui, QtWidgets, QtCore
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # settings window MainWindow
+        self.setWindowTitle("PCt")
+        self.setFixedSize(QSize(800, 600)) # size window
+        self.label = QLabel("", self)
+        font = QtGui.QFont()
+        font.setFamily('CeraPro-Bold')  # font
+        font.setPointSize(14)  # size of font шрифта
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec()
