099785ccb82fa141d4e4eb2a279219a213498cc7
Andrey_Panyshkin <86359412+Santas7@users.noreply.github.com>
2023-03-05 18:40:26 +0400

    Adding the stylization of the main application window

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ class MainWindow(QMainWindow):
         super().__init__()
         # settings window MainWindow
         self.setWindowTitle("PCt")
-        self.setFixedSize(QSize(800, 600)) # size window
+        self.setFixedSize(QSize(380, 500)) # size window
         self.title = QLabel("PCt", self)
-
+        self.setStyleSheet("background-color : black")
         # standart text
         font = QtGui.QFont()
         font.setFamily('CeraPro-Bold')  # font
         font.setPointSize(14)  # size of font
+
 
         # title text
         font_ = QtGui.QFont()
@@ -30,26 +31,43 @@ class MainWindow(QMainWindow):
 
         # change for title
         self.title.setFont(font_)
-        self.title.move(380, 50)
+        self.title.move(160, 50)
+        self.title.setStyleSheet("color: white")
 
         # panel input user name
         self.input_user = QLineEdit('', self)
-        self.input_user.move(200, 110)
-        self.input_user.setFixedSize(QSize(400, 40))
+        self.input_user.move(10, 110)
+        self.input_user.setFixedSize(QSize(360, 40))
         self.input_user.setFont(font)
+        self.input_user.setStyleSheet("color: white")
 
         # panel input ip
-        self.input_user = QLineEdit('', self)
-        self.input_user.move(200, 160)
-        self.input_user.setFixedSize(QSize(400, 40))
-        self.input_user.setFont(font)
+        self.input_ip = QLineEdit('', self)
+        self.input_ip.move(10, 160)
+        self.input_ip.setFixedSize(QSize(360, 40))
+        self.input_ip.setFont(font)
+        self.input_ip.setStyleSheet("color: white")
 
         # panel input password connect
-        self.input_user = QLineEdit('', self)
-        self.input_user.move(200, 210)
-        self.input_user.setFixedSize(QSize(400, 40))
-        self.input_user.setFont(font)
-        
+        self.input_pass = QLineEdit('', self)
+        self.input_pass.move(10, 210)
+        self.input_pass.setFixedSize(QSize(360, 40))
+        self.input_pass.setFont(font)
+        self.input_pass.setStyleSheet("color: white")
+
+        # buttons
+        self.start = QPushButton("CONNECT TO", self)
+        self.start.setFixedSize(QSize(150, 50))
+        self.start.move(10, 260)
+        self.start.setFont(font)
+        self.start.setStyleSheet("background-color : #ff5757; color: white")
+
+        self.end = QPushButton("EXIT..", self)
+        self.end.setFixedSize(QSize(150, 50))
+        self.end.move(170, 260)
+        self.end.setFont(font)
+        self.end.setStyleSheet("background-color : #ff5757; color: white")
+
         self.show()
 
 if __name__ == "__main__":
